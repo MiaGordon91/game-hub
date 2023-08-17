43dd4e9d301be3121ca0e25ce7b6f036febfe2c3
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2023-08-17 16:58:41 +0100

    create responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  return <Grid
+  templateAreas={
+    {base: `"header"
+            "main"
+            "footer"`,
+      lg:`"header header"
+      "aside main"
+      "aside footer"`}
+  }
+  // gridTemplateRows={'50px 1fr 30px'}
+  // gridTemplateColumns={'150px 1fr'}
+  // h='900px'
+  gap='2'
+  color='blackAlpha.700'
+  fontWeight='bold'
+>
+  <GridItem pl='2' bg='#000000' area={'header'} color='#FFFFFF'>
+    Header
+  </GridItem>
+  <GridItem pl='2' bg='#808080' area={'aside'}>
+    Aside
+  </GridItem>
+  <GridItem pl='2' bg='#FFFFFF' area={'main'}>
+    Main
+  </GridItem>
+  <GridItem pl='2' bg='#A9A9A9' area={'footer'}>
+    Footer
+  </GridItem>
+</Grid>
 
 }
 
