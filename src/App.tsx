7a0c3749a4dc75ba01b1f565a20554af5a4faf99
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2023-08-24 13:30:25 +0100

    build simple navigation bar with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
+
 
 function App() {
   return <Grid
@@ -10,25 +12,26 @@ function App() {
       "aside main"
       "aside footer"`}
   }
-  // gridTemplateRows={'50px 1fr 30px'}
-  // gridTemplateColumns={'150px 1fr'}
-  // h='900px'
+  gridTemplateColumns={'150px 1fr'}
   gap='2'
   color='blackAlpha.700'
   fontWeight='bold'
 >
-  <GridItem pl='2' bg='#000000' area={'header'} color='#FFFFFF'>
-    Header
+  <GridItem pl='2' area={'header'}>
+    <NavBar />
   </GridItem>
+
+  <Show above="lg">
   <GridItem pl='2' bg='#808080' area={'aside'}>
     Aside
   </GridItem>
-  <GridItem pl='2' bg='#FFFFFF' area={'main'}>
+  <GridItem pl='2' bg='#A9A9A9' area={'main'}>
     Main
   </GridItem>
   <GridItem pl='2' bg='#A9A9A9' area={'footer'}>
     Footer
   </GridItem>
+  </Show>
 </Grid>
 
 }
